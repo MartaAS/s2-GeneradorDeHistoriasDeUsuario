80e46a120c3a6d4fd82196b2ca2d67544541e741
Marta Aguilar <martaguilar.soto@gmail.com>
2017-11-29 09:30:51 +0100

    prompt first section

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
 'use strict';
+var usuario = prompt ('Dime tu usuario de instagram');
+var funcionalidad = prompt ('¿Qué necesitas de instagram?');
+var razon = prompt ('¿Con qué finalidad?');
+alert('mi usuario de instagram es ' + usuario + ', necesito ' + funcionalidad + ', con la finalidad de ' + razon);
